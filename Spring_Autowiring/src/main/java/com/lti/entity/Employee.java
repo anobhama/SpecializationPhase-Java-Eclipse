1c33450751dc13547e4ac29ee7eaadd01cb9e8ff
Anobhama <anobhama99.com>
2021-09-02 16:43:58 +0530

    Spring- Understanding autowiring

---
+++ Spring_Autowiring/src/main/java/com/lti/entity/Employee.java
@@ -0,0 +1,66 @@
+package com.lti.entity;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Employee {
+	
+	private int empId;
+	private String empName;
+	
+	//calling address class here
+	private Address address;
+	
+	public Employee() {
+		super();
+	}
+
+	public Employee(int empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+
+	
+	public Employee(int empId, String empName, Address address) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.address = address;
+	}
+
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", address=" + address + "]";
+	}
+
+
+	
+	}
+
